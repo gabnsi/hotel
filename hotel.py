d5edc12773d2070f8f70f3193aad0fb8ac752849
adriennnnnn <112926730+adriennnnnn@users.noreply.github.com>
2022-12-01 15:13:21 +0100

    Add files via upload

--- hotel.py
+++ hotel.py
@@ -3,7 +3,8 @@ fichier_bdd = "bdd.db"
 conn = sqlite3.connect(fichier_bdd)
 cur = conn.cursor()
 
-req = "INSERT INTO client (nom, prenom) VALUES(?, ?)"
+req1 = "INSERT INTO client (nom, prenom) VALUES(?, ?)"
+req2 = "INSERT INTO chambres (capacité, tarif) VALUES(?, ?)"
 
 from random import choice
 
@@ -29,12 +30,42 @@ NOMS = (
   "Mejda"
 )
 
-for i in range(10):
-    p = choice(PRENOMS)
-    n = choice(NOMS)
-    cur.execute(req, (n, p))
+CAPACITE = (
+  1,
+  2,
+  3,
+  4,
+  5,
+  6
+)
+
+PRIX = (
+  70,
+  80,
+  100,
+  150,
+  200
+)
+
+#for i in range(10):
+#    p = choice(PRENOMS)
+#    n = choice(NOMS)
+#    cur.execute(req1, (n, p))
+
+#for i in range(50):
+#  c = choice(CAPACITE)
+#  p = choice(PRIX)
+#  cur.execute(req2, (c, p))
+
 
 conn.commit()
 
 cur.close()
 conn.close()
+
+class Query:
+  ajouter_client = [
+    '''
+    INSERT INTO client (nom, prenom) VALUES(?, ?)
+    '''
+  ]

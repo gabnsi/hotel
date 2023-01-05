a80fcdd2bbfdaaabf0e8584762ae8aa4792966b8
gabnsi <112926821+gabnsi@users.noreply.github.com>
2023-01-05 23:20:45 +0100

    Add files via upload

---
+++ serveur.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from clients import Reservation, Client, liste_client, liste_chambres
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    clients = liste_client()
+    chambres = liste_chambres()
+    return render_template("index.html", clients=clients, chambres=chambres)
+
+@app.route('/ajouter_client', methods=['POST'])
+def ajouter_client():
+    result = request.values  # méthode POST
+    Nom = result['nom']
+    Prenom = result['prenom']
+    client = Client(None, Nom, Prenom)
+    client.inserer_client()
+    return render_template("ajouter_client.html",
+                           nom=Nom,
+                           prenom=Prenom)
+
+@app.route('/supprimer_client', methods=['POST'])
+def supprimer_client():
+    result = request.values  # méthode POST
+    Nom = result['nom']
+    Prenom = result['prenom']
+    client = Client(None, Nom, Prenom)
+    client.supprimer_client()
+    return render_template("supression_client.html",
+                           nom=Nom,
+                           prenom=Prenom)
+
+
+@app.route('/reserver', methods=['POST'])
+def Reserver():
+    result = request.values  # méthode POST
+    id_client=result['id_client']
+    id_chambre=result['id_chambre']
+    date_debut=result['date_debut']
+    date_fin=result['date_fin']
+    reservation = Reservation(None, id_client, id_chambre, date_debut, date_fin)
+    reservation.reserver()
+    return render_template("resultat.html",
+                           id_client=id_client,
+                           id_chambre=id_chambre,
+                           date_debut=date_debut,
+                           date_fin=date_fin,
+                           )
+
+
+app.run(debug=True, port=3579)
